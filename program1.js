8d08b7bdeb714cea7100fa9f305170c427c8f36f
Sandeep3jarma <sandeepjarma3@gmail.com>
2024-07-26 13:19:19 +0530

    add

--- program1.js
+++ program1.js
@@ -1,8 +1,56 @@
-const getTotalIsles = function (grid) {
+// const getTotalIsles = function (grid) {
 
 
-  // write your code here
+//   // write your code here
+  
 
-};
+// };
+
+// module.exports = getTotalIsles;
+ 
+// program1.js
+function getTotalIsles(grid) {
+  if (grid.length === 0) return 0;
+
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+  const directions = [
+      [1, 0], // down
+      [-1, 0], // up
+      [0, 1], // right
+      [0, -1] // left
+  ];
+
+  function isValid(r, c) {
+      return r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] === 'L' && !visited[r][c];
+  }
+
+  function dfs(r, c) {
+      visited[r][c] = true;
+      for (const [dr, dc] of directions) {
+          const newRow = r + dr;
+          const newCol = c + dc;
+          if (isValid(newRow, newCol)) {
+              dfs(newRow, newCol);
+          }
+      }
+  }
+
+  let islandCount = 0;
+
+  for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+          if (grid[r][c] === 'L' && !visited[r][c]) {
+              dfs(r, c);
+              islandCount++;
+          }
+      }
+  }
+
+  return islandCount;
+}
 
 module.exports = getTotalIsles;
+
--- program2.js
+++ program2.js
@@ -1,7 +1,14 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
+    function decodeTheRing(str, pattern) {
+      const regex = new RegExp('^' + pattern.replace(/\*/g, '.*').replace(/\?/g, '.') + '$');
+      return regex.test(str);
+  }
+  
+  
 
   };
   
   module.exports = decodeTheRing;
+
